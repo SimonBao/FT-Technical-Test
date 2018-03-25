601301ec6c603a19df694044defdda1dc7e8f086
SimonBao <Simonbao.sb@gmail.com>
2018-03-25 23:37:08 +0100

    Refactored 'rating' to 'score' for clarity. Therefore 'Rating.rating' becomes 'Rating.score'.

--- spec/features/rating_spec.rb
+++ spec/features/rating_spec.rb
@@ -1,5 +1,6 @@
 describe Rating do
   it 'creates new rating' do
-    expect{Rating.create(rating: 5)}.to change{Rating.count}.by 1
+    expect{Rating.create(score: 5)}.to change{Rating.count}.by 1
   end
+
 end
--- spec/features/review_page_spec.rb
+++ spec/features/review_page_spec.rb
@@ -1,7 +1,7 @@
 feature 'For User Feedback' do
   scenario 'They can rate website' do
     visit '/'
-    fill_in :rating, with: 2
+    fill_in :score, with: 2
     click_button 'Submit'
   end
 end
